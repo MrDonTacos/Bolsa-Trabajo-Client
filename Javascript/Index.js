1efadeead28c5a0e1197cd6c4f14bdd3c569dc56
Saul Canedo Castillo <saul.canedocast@hotmail.com>
2020-11-04 08:29:02 -0800

    First Commit

---
+++ Javascript/Index.js
@@ -0,0 +1,68 @@
+const url = 'http://localhost:5000/api/Tienda_Musica/'
+let viewModel = null;
+$(document).ready(() => {
+//     cargarArtistas();
+//     cargarArtista();
+// });
+
+// const cargarArtista = () => {
+//     query(`${url}Name`).then((data) => {
+//       viewModel.artistas(data);
+//     })
+// }
+//         const cargarArtistas= () => {
+
+            
+//             viewModel = {
+//                 artists: ko.observableArray([])
+//             };
+
+
+//             viewModel.artistas = function (data) {
+//                 let artists = data.map( (g) => { return g;});
+//                 artists.unshift("Artistas");
+//                 viewModel.artists(artists);
+                
+//             }
+//             ko.applyBindings(viewModel);
+        
+$("#teloficina").kendoComboBox({
+    dataSource: dataSourceEmpleadoCurso,
+    dataTextField: "nom_curso",
+    dataValueField: "id_curso"
+})
+
+$("#teloficina").kendoComboBox({
+    dataSource: dataSourceEmpleadoCurso,
+    dataTextField: "nom_curso",
+    dataValueField: "id_curso"
+})
+
+$("#teloficina").kendoComboBox({
+    dataSource: dataSourceEmpleadoCurso,
+    dataTextField: "nom_curso",
+    dataValueField: "id_curso"
+})
+
+$("#teloficina").kendoComboBox({
+    dataSource: dataSourceEmpleadoCurso,
+    dataTextField: "nom_curso",
+    dataValueField: "id_curso"
+})
+
+$("#teloficina").kendoComboBox({
+    dataSource: dataSourceEmpleadoCurso,
+    dataTextField: "nom_curso",
+    dataValueField: "id_curso"
+})
+
+$("#teloficina").kendoComboBox({
+    dataSource: dataSourceEmpleadoCurso,
+    dataTextField: "nom_curso",
+    dataValueField: "id_curso"
+})
+});
+
+        
+
+
