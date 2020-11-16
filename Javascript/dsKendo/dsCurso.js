b6863c2d68034c1704b11d269002d8ec044ed26a
Giovanni <giosave98@gmail.com>
2020-11-16 00:40:18 -0800

    Add class for 'curso' consuming data GET

---
+++ Javascript/dsKendo/dsCurso.js
@@ -0,0 +1,48 @@
+var dataSourceCurso = new kendo.data.DataSource({
+    transport: {
+        read: {
+            cache: false,
+            type: "GET",
+            url: function (e) {
+                let url = `${baseurl}/curso`;
+                return url;
+            },
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+        },
+        parameterMap: function (options, operation) {
+            if (operation !== "read" && options) {
+                return kendo.stringify(options);
+            }
+        }
+    },
+    schema: {
+        errors: "error",
+        data: function (data) {
+            return data;
+        },
+        total: function (data) {
+            return data.length;
+        },
+        model: {
+            id: "id_documento",
+            fields: {
+                id_curso: { type: "string"},
+                nom_curso: {
+                    type: "string", title: 'Nombre',
+                    validation: {
+                        required: true,
+                        maxlength: 250
+                    }
+                },
+            }
+        }
+    },
+    error: function (e) {
+        debugger
+        console.log("error:" + e);
+        alert(e.xhr.responseText + " error");
+    }
+});
+
+console.log("HOLA MUNDO");
