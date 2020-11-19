ce86d31b246bf0ca82d2e1fa0b7e85961e64eedc
Giovanni <giosave98@gmail.com>
2020-11-19 00:06:07 -0800

    Wrap in function for button

--- secciones/curso/POSTCurso.js
+++ secciones/curso/POSTCurso.js
@@ -1,27 +1,30 @@
-const UrlCurso = 'http://localhost:5000/api/curso/';
+
+function botonGuardar() {
+    const UrlCurso = 'http://localhost:5000/api/curso/';
 
 var formulario = document.getElementById('loginCurso');
 
-formulario.addEventListener('submit', function(e){
-    e.preventDefault();
-
-    var datos = new FormData(formulario);
-
-    fetch(UrlCurso,{
-        method: 'POST',
-        //contentType:'application/json',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            id_curso: datos.get('claveCurso'),
-            nom_curso: datos.get('nombreCurso')
+    formulario.addEventListener('submit', function(e){
+        e.preventDefault();
+    
+        var datos = new FormData(formulario);
+    
+        fetch(UrlCurso,{
+            method: 'POST',
+            //contentType:'application/json',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id_curso: datos.get('claveCurso'),
+                nom_curso: datos.get('nombreCurso')
+            })
         })
-    })
-    .then( res => res.json())
-    .then( data => {
-        Swal.fire(data);
-        console.log(data);
+        .then( res => res.json())
+        .then( data => {
+            Swal.fire(data);
+            console.log(data);
+        });
     });
-});
+}
