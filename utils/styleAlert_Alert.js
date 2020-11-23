74bf71a0d2a963fceb444e9433fbeed40b11602b
Giovanni <giosave98@gmail.com>
2020-11-22 20:44:20 -0800

    Commet alert with conditions

--- utils/styleAlert_Alert.js
+++ utils/styleAlert_Alert.js
@@ -10,3 +10,20 @@ $('#btnPromt').click(function(){
           cancelButtonText: "Cancelar"
         });
 });
+
+  /* Read more about isConfirmed, isDenied below
+///alerta de si o no
+Swal.fire({
+  title: 'Do you want to save the changes?',
+  showDenyButton: true,
+  showCancelButton: true,
+  confirmButtonText: `Save`,
+  denyButtonText: `Don't save`,
+}).then((result) => {
+
+  if (result.isConfirmed) {
+    Swal.fire('Saved!', '', 'success')
+  } else if (result.isDenied) {
+    Swal.fire('Changes are not saved', '', 'info')
+  }
+}) */
